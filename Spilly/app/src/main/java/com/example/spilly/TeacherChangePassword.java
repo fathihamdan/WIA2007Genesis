6ecfca91e329cab0d4831511f2afb779c95d7a32
Fahim <05muhdfahim@gmail.com>
2026-01-02 14:42:38 +0800

    done

--- Spilly/app/src/main/java/com/example/spilly/TeacherChangePassword.java
+++ Spilly/app/src/main/java/com/example/spilly/TeacherChangePassword.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ViewReportPage#newInstance} factory method to
+ * Use the {@link TeacherChangePassword#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ViewReportPage extends Fragment {
+public class TeacherChangePassword extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class ViewReportPage extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public ViewReportPage() {
+    public TeacherChangePassword() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class ViewReportPage extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment ViewReportPage.
+     * @return A new instance of fragment TeacherChangePassword.
      */
     // TODO: Rename and change types and number of parameters
-    public static ViewReportPage newInstance(String param1, String param2) {
-        ViewReportPage fragment = new ViewReportPage();
+    public static TeacherChangePassword newInstance(String param1, String param2) {
+        TeacherChangePassword fragment = new TeacherChangePassword();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class ViewReportPage extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_report_page, container, false);
+        return inflater.inflate(R.layout.fragment_teacher_change_password, container, false);
     }
 }
